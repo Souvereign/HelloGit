79d701fb07af29a9c0e81578ef8af95ecf8d0bfe
fof <fof@qq.com>
2020-10-06 16:00:18 +0800

    branch

--- HelloGit/src/main/java/HelloWorld.java
+++ HelloGit/src/main/java/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
         System.out.println("Hello Git");
         System.out.println("Hello Java");
         System.out.println("Hello C++");
+        System.out.println("Hello C");
     }
 }
